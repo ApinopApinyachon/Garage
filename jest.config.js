c9e4ef6c34e2d4f04714c2e931f318dd2ade0a53
ApinopApinyachon <152172069+ApinopApinyachon@users.noreply.github.com>
2025-03-21 22:21:06 +0700

    Fix Codeclimatee

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     collectCoverage: true,
     coverageDirectory: "coverage",
     coverageReporters: ["json", "lcov", "text", "clover"],
+    transform: {
+      "^.+\\.(js|jsx)$": "babel-jest"
+    }
   };
   
